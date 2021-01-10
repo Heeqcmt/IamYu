51b55ff5f3745430c6998a53d76650a19fec44c1
YuMing Zhang <yumingzhang94@gmail.com>
2021-01-09 19:48:15 -0500

    slight adjustment

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,13 @@
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import react from 'react';
+import './components.css'
 
 
 export default function Header() 
 {
     return (
-        <Jumbotron>
-            <h1> I am Yu</h1>
+        <Jumbotron className="banner">
+            <h1 > I am Yu</h1>
         </Jumbotron>
 
     );
